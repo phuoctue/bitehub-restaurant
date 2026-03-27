3078cbf652c8ee33a7b3f3b62aec3b5dc6b93d66
Hell-Haven <trivo2k0@gmail.com>
2026-03-27 15:51:22 +0700

    Done Chương 9

--- client/src/apiRequest/account.ts
+++ client/src/apiRequest/account.ts
@@ -1,16 +1,42 @@
-import http from "@/lib/http";
+import http from '@/lib/http'
+
 import {
+  AccountListResType,
   AccountResType,
   ChangePasswordBodyType,
-  UpdateMeBodyType,
-} from "@/schemaValidations/account.schema";
+  CreateEmployeeAccountBodyType,
+  CreateGuestBodyType,
+  UpdateEmployeeAccountBodyType,
+  UpdateMeBodyType
+} from '@/schemaValidations/account.schema'
+import { get } from 'http'
+import { de } from 'zod/v4/locales'
+
+const prefix = '/accounts'
 
 const accountApiRequest = {
-  me: () => http.get<AccountResType>("/accounts/me"),
-  updatMe: (body: UpdateMeBodyType) =>
-    http.put<AccountResType>("/accounts/me", body),
-  changPassword: (body: ChangePasswordBodyType) =>
-    http.put<AccountResType>("/accounts/change-password", body),
-};
+  me: () => http.get<AccountResType>(`${prefix}/me`),
 
-export default accountApiRequest;
+  sMe: (accessToken: string) =>
+    http.get<AccountResType>(`${prefix}/me`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }),
+
+  updateMe: (body: UpdateMeBodyType) =>
+    http.put<AccountResType>(`${prefix}/me`, body),
+
+  changePassword: (body: ChangePasswordBodyType) =>
+    http.put<AccountResType>(`${prefix}/change-password`, body),
+
+  list: () => http.get<AccountListResType>(`${prefix}`),
+
+  addEmployee: (body: CreateEmployeeAccountBodyType) =>
+    http.post<AccountResType>(`${prefix}`, body),
+  updateEmployee: (id: number, body: UpdateEmployeeAccountBodyType) => http.put<AccountResType>(`${prefix}/detail/${id}`, body),
+  getEmployee: (id: number) => http.get<AccountResType>(`${prefix}/detail/${id}`),
+  deleteEmployee: (id: number) => http.delete<AccountResType>(`${prefix}/detail/${id}`),
+}
+
+export default accountApiRequest
