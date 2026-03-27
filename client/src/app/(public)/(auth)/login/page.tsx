b58a85172b1f1382d4a7116090a6091424e0f600
phuoctue <phuoctue0312@gmail.com>
2026-03-27 19:26:01 +0700

    update DB cloud

--- client/src/app/(public)/(auth)/login/page.tsx
+++ client/src/app/(public)/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from '@/app/(public)/(auth)/login/login-form'
+import { Suspense } from 'react'
 
 export default function Login() {
   return (
     <div className='min-h-[calc(100vh-12rem)] flex flex-col items-center justify-center px-4'>
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   )
 }
